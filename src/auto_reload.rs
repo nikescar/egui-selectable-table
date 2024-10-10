c76e1cff22b139255f889da014c7087052e4f4c3
Rusty Pickle <rusty.pickle94@gmail.com>
2024-10-10 14:58:17 +0600

    Fix auto reload not resetting

--- src/auto_reload.rs
+++ src/auto_reload.rs
@@ -4,14 +4,18 @@ use crate::{ColumnOperations, ColumnOrdering, SelectableTable};
 
 #[derive(Default)]
 pub struct AutoReload {
-    reload_after: Option<u32>,
-    reload_count: u32,
+    pub reload_after: Option<u32>,
+    pub reload_count: u32,
 }
 impl AutoReload {
     pub(crate) fn increment_count(&mut self) -> bool {
         self.reload_count += 1;
         if let Some(count) = self.reload_after {
-            self.reload_count >= count
+            let reload = self.reload_count >= count;
+            if reload {
+                self.reload_count = 0;
+            }
+            reload
         } else {
             false
         }
@@ -35,10 +39,12 @@ where
     #[must_use]
     pub const fn auto_reload(mut self, count: u32) -> Self {
         self.auto_reload.reload_after = Some(count);
+        self.auto_reload.reload_count = 0;
         self
     }
 
     pub fn set_auto_reload(&mut self, count: Option<u32>) {
         self.auto_reload.reload_after = count;
+        self.auto_reload.reload_count = 0;
     }
 }
